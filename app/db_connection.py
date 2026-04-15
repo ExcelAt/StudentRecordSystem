16fca950a6c52248ffae9a705b685eba5d924edb
Lethukuthula Mthiyyane <201454130.ln@gmail.com>
2026-04-15 23:02:32 +0200

    Add app folder with db_connection helper and gitignore

---
+++ app/db_connection.py
@@ -0,0 +1,15 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def get_connection():
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        dbname=os.getenv("DB_NAME"),
+        sslmode=os.getenv("DB_SSLMODE")
+    )
+    return conn
