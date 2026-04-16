a18152387666187afdf98656e43095a2704583d5
pinkie2849 <sibongile.maluleka90@gmail.com>
2026-04-16 11:57:56 +0200

    transform and load process

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -33,3 +33,60 @@ def extract(file_path):
         return pd.read_json(file_path)
     else:
         raise ValueError("Unsupported file format")
+    
+    # ---------------- TRANSFORM ----------------
+def clean_columns(df):
+    df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
+    return df
+
+def handle_missing(df):
+    return df.fillna({
+        "email": "unknown@email.com"
+    })
+
+def validate_email(email):
+    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    return bool(re.match(pattern, str(email)))
+
+def transform_students(df):
+    df = clean_columns(df)
+    df = handle_missing(df)
+
+    df["name"] = df["name"].str.title()
+    df["surname"] = df["surname"].str.title()
+    df["email"] = df["email"].str.lower()
+
+    df = df[df["email"].apply(validate_email)]
+    return df
+
+def transform_grades(df):
+    df = clean_columns(df)
+
+    # GPA calculation
+    df["gpa"] = (df["assignment_grade"] + df["exam_grade"]) / 2 / 20
+    return df
+
+def transform_attendance(df):
+    df = clean_columns(df)
+
+    # Attendance analysis
+    df["low_attendance"] = df["attendance_percentage"] < 75
+    return df
+
+# ---------------- LOAD ----------------
+def load_data(query, data):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    try:
+        cursor.executemany(query, data)
+        conn.commit()
+        logging.info("Data loaded successfully")
+
+    except Exception as e:
+        conn.rollback()
+        logging.error(f"Load failed: {e}")
+
+    finally:
+        cursor.close()
+        conn.close()
