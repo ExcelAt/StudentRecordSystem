67369b0063999f009ce31c03677a8e4e4e161bea
Sibongile Maluleka <sibongile.maluleka90@gmail.com>
2026-04-16 11:45:56 +0200

    Logs,connection and extracting

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -11,3 +11,25 @@ DB_CONFIG = {
     "dbname": "student_record_system",
     "sslmode": "require"
 }
+
+# ---------------- LOGGING ----------------
+logging.basicConfig(
+    filename="etl_pipeline.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+# ---------------- CONNECT ----------------
+def get_connection():
+    return psycopg2.connect(**DB_CONFIG)
+
+# ---------------- EXTRACT ----------------
+def extract(file_path):
+    if file_path.endswith(".csv"):
+        return pd.read_csv(file_path)
+    elif file_path.endswith(".xlsx"):
+        return pd.read_excel(file_path)
+    elif file_path.endswith(".json"):
+        return pd.read_json(file_path)
+    else:
+        raise ValueError("Unsupported file format")
